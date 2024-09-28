9c1183b652d35e06675db448ddf2a0a99c05119e
stackgen-automation <automation@stackgen.com>
2024-09-28 10:06:49 +0000

    Automated commit

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -11,38 +11,26 @@ output "aws_cloudwatch_log_group_aws_lambda_mysterio_name" {
 }
 
 
-output "aws_s3_aws_lambdagoblin_mysterio_arn" {
-  value = module.appcd_6d0d835b-04d1-4911-a468-a09a3ca69c65.arn
-  sensitive = false
-}
-
-
-output "aws_s3_aws_lambdagoblin_mysterio_bucket_name" {
+output "aws_s3_aws_lambda_goblin_mysterio_bucket_name" {
   value = module.appcd_6d0d835b-04d1-4911-a468-a09a3ca69c65.bucket_name
   sensitive = false
 }
 
 
-output "aws_s3_aws_lambdagoblin_mysterio_bucket_website_endpoint" {
+output "aws_s3_aws_lambda_goblin_mysterio_bucket_website_endpoint" {
   value = module.appcd_6d0d835b-04d1-4911-a468-a09a3ca69c65.bucket_website_endpoint
   sensitive = false
 }
 
 
-output "aws_s3_aws_lambdagoblin_mysterio_kms_arn" {
+output "aws_s3_aws_lambda_goblin_mysterio_kms_arn" {
   value = module.appcd_6d0d835b-04d1-4911-a468-a09a3ca69c65.kms_arn
   sensitive = false
 }
 
 
-output "aws_lambda_aws_lambda_mysterio_function_arn" {
-  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_arn
-  sensitive = false
-}
-
-
-output "aws_lambda_aws_lambda_mysterio_function_name" {
-  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_name
+output "aws_s3_aws_lambda_goblin_mysterio_arn" {
+  value = module.appcd_6d0d835b-04d1-4911-a468-a09a3ca69c65.arn
   sensitive = false
 }
 
@@ -55,6 +43,18 @@ output "aws_lambda_aws_lambda_mysterio_function_url" {
 
 output "aws_lambda_aws_lambda_mysterio_invoke_arn" {
   value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.invoke_arn
+  sensitive = false
+}
+
+
+output "aws_lambda_aws_lambda_mysterio_function_arn" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_arn
+  sensitive = false
+}
+
+
+output "aws_lambda_aws_lambda_mysterio_function_name" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_name
   sensitive = false
 }
 
