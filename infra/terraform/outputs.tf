21e886336fb8df2fcec1d83b1482fcd02937dfbc
stackgen-automation <automation@stackgen.com>
2024-09-28 09:47:26 +0000

    Automated commit

---
+++ infra/terraform/outputs.tf
@@ -0,0 +1,71 @@
+
+output "aws_lambda_goblin_function_arn" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_arn
+  sensitive = false
+}
+
+
+output "aws_lambda_goblin_function_name" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_name
+  sensitive = false
+}
+
+
+output "aws_lambda_goblin_function_url" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_url
+  sensitive = false
+}
+
+
+output "aws_lambda_goblin_invoke_arn" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.invoke_arn
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_mysterio_arn" {
+  value = module.appcd_86ec452a-770a-59bf-87dd-67ed883e25e5.arn
+  sensitive = false
+}
+
+
+output "aws_cloudwatch_log_group_mysterio_name" {
+  value = module.appcd_86ec452a-770a-59bf-87dd-67ed883e25e5.name
+  sensitive = false
+}
+
+
+output "aws_s3_mysterio_bucket_website_endpoint" {
+  value = module.appcd_6d0d835b-04d1-4911-a468-a09a3ca69c65.bucket_website_endpoint
+  sensitive = false
+}
+
+
+output "aws_s3_mysterio_kms_arn" {
+  value = module.appcd_6d0d835b-04d1-4911-a468-a09a3ca69c65.kms_arn
+  sensitive = false
+}
+
+
+output "aws_s3_mysterio_arn" {
+  value = module.appcd_6d0d835b-04d1-4911-a468-a09a3ca69c65.arn
+  sensitive = false
+}
+
+
+output "aws_s3_mysterio_bucket_name" {
+  value = module.appcd_6d0d835b-04d1-4911-a468-a09a3ca69c65.bucket_name
+  sensitive = false
+}
+
+
+output "aws_iam_role_aws_lambda_mysterio_arn" {
+  value = module.appcd_b71fd16c-797e-5a95-8e1b-ab22f1e34f43.arn
+  sensitive = false
+}
+
+
+output "aws_iam_role_aws_lambda_mysterio_name" {
+  value = module.appcd_b71fd16c-797e-5a95-8e1b-ab22f1e34f43.name
+  sensitive = false
+}
