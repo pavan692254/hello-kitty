ba71709ee84d7dba63018afb67f0c0589b212fc5
stackgen-automation <automation@stackgen.com>
2024-09-28 09:56:48 +0000

    Automated commit

--- infra/terraform/outputs.tf
+++ infra/terraform/outputs.tf
@@ -1,6 +1,12 @@
 
-output "aws_lambda_goblin_function_arn" {
-  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_arn
+output "aws_iam_role_aws_lambda_mysterio_arn" {
+  value = module.appcd_b71fd16c-797e-5a95-8e1b-ab22f1e34f43.arn
+  sensitive = false
+}
+
+
+output "aws_iam_role_aws_lambda_mysterio_name" {
+  value = module.appcd_b71fd16c-797e-5a95-8e1b-ab22f1e34f43.name
   sensitive = false
 }
 
@@ -23,6 +29,12 @@ output "aws_lambda_goblin_invoke_arn" {
 }
 
 
+output "aws_lambda_goblin_function_arn" {
+  value = module.appcd_00a20b0a-2ec0-5bdc-a08d-65a85dc81d5b.function_arn
+  sensitive = false
+}
+
+
 output "aws_cloudwatch_log_group_mysterio_arn" {
   value = module.appcd_86ec452a-770a-59bf-87dd-67ed883e25e5.arn
   sensitive = false
@@ -35,37 +47,25 @@ output "aws_cloudwatch_log_group_mysterio_name" {
 }
 
 
-output "aws_s3_mysterio_bucket_website_endpoint" {
-  value = module.appcd_6d0d835b-04d1-4911-a468-a09a3ca69c65.bucket_website_endpoint
-  sensitive = false
-}
-
-
-output "aws_s3_mysterio_kms_arn" {
-  value = module.appcd_6d0d835b-04d1-4911-a468-a09a3ca69c65.kms_arn
-  sensitive = false
-}
-
-
-output "aws_s3_mysterio_arn" {
+output "aws_s3_goblin_mysterio_arn" {
   value = module.appcd_6d0d835b-04d1-4911-a468-a09a3ca69c65.arn
   sensitive = false
 }
 
 
-output "aws_s3_mysterio_bucket_name" {
+output "aws_s3_goblin_mysterio_bucket_name" {
   value = module.appcd_6d0d835b-04d1-4911-a468-a09a3ca69c65.bucket_name
   sensitive = false
 }
 
 
-output "aws_iam_role_aws_lambda_mysterio_arn" {
-  value = module.appcd_b71fd16c-797e-5a95-8e1b-ab22f1e34f43.arn
+output "aws_s3_goblin_mysterio_bucket_website_endpoint" {
+  value = module.appcd_6d0d835b-04d1-4911-a468-a09a3ca69c65.bucket_website_endpoint
   sensitive = false
 }
 
 
-output "aws_iam_role_aws_lambda_mysterio_name" {
-  value = module.appcd_b71fd16c-797e-5a95-8e1b-ab22f1e34f43.name
+output "aws_s3_goblin_mysterio_kms_arn" {
+  value = module.appcd_6d0d835b-04d1-4911-a468-a09a3ca69c65.kms_arn
   sensitive = false
 }
